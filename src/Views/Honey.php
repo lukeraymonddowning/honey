f1fab103e7d85ff60292f9b3774162e0a47070da
Adriaan Zonnenberg <amz@adriaan.xyz>
2021-01-14 22:15:21 +0100

    Don't render script tag when JavaScript check is disabled

--- src/Views/Honey.php
+++ src/Views/Honey.php
@@ -20,23 +20,25 @@ class Honey extends Component
     public function render()
     {
         return <<<'blade'
-                @once
-                    <script>
-                        window.addEventListener('load', () => {
-                            setTimeout(() => {
-                                document.querySelectorAll('input[data-purpose="{{ $inputNameSelector->getJavascriptInputName() }}"]')
-                                    .forEach(input => {
-                                        if (input.value.length > 0) {
-                                            return;
-                                        }
-                                        
-                                        input.value = "{{ $javascriptValue() }}";
-                                        input.dispatchEvent(new Event('change'));
-                                    });
-                            }, {{ $javascriptTimeout() }})
-                        });
-                    </script>
-                @endonce
+               @if (in_array(\Lukeraymonddowning\Honey\Checks\JavascriptInputFilledCheck::class, config('honey.checks')))
+                    @once
+                        <script>
+                            window.addEventListener('load', () => {
+                                setTimeout(() => {
+                                    document.querySelectorAll('input[data-purpose="{{ $inputNameSelector->getJavascriptInputName() }}"]')
+                                        .forEach(input => {
+                                            if (input.value.length > 0) {
+                                                return;
+                                            }
+                                            
+                                            input.value = "{{ $javascriptValue() }}";
+                                            input.dispatchEvent(new Event('change'));
+                                        });
+                                }, {{ $javascriptTimeout() }})
+                            });
+                        </script>
+                    @endonce
+                @endif
                 <div style="display: @isset($attributes['debug']) block @else none @endisset;">
                     <input wire:model.lazy.defer="honeyInputs.{{ $inputNameSelector->getPresentButEmptyInputName() }}" name="{{ $inputNameSelector->getPresentButEmptyInputName() }}" value="">
                     <input wire:model.lazy.defer="honeyInputs.{{ $inputNameSelector->getTimeOfPageLoadInputName() }}" name="{{ $inputNameSelector->getTimeOfPageLoadInputName() }}" value="{{ $timeOfPageLoadValue() }}">
