b4232edcfd07f120a69897418c9aa12cbc219805
Antonio Mora Rives <antoniomorarives@gmail.com>
2022-06-27 08:50:39 +0200

    fix: :bug: fixed undefined array key "honey_time" error
    #33 #59

--- src/Checks/MinimumTimePassedCheck.php
+++ src/Checks/MinimumTimePassedCheck.php
@@ -9,8 +9,21 @@ use Lukeraymonddowning\Honey\InputValues\Values;
 
 class MinimumTimePassedCheck implements Check
 {
+    protected $data;
+
+    protected function missingFromData()
+    {
+        return !$this->data->offsetExists(Honey::inputs()->getTimeOfPageLoadInputName());
+    }
+
     public function passes($data)
     {
+        $this->data = collect($data);
+
+        if ($this->missingFromData()) {
+            return false;
+        }
+
         $value = $data[Honey::inputs()->getTimeOfPageLoadInputName()];
         return rescue(fn() => Values::timeOfPageLoad()->checkValue($value));
     }
